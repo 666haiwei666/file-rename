8a9d3b707428c559eb96a3e6140424633786573d
aline.chen <aline.chen@dbappsecurity.com.cn>
2021-12-26 16:02:39 +0800

    feature: 部分功能开发

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,13 @@
 #! /usr/bin/env node
 const path = require('path')
 const { Command } = require('commander')
-const { checkNodeVersion } = require('../lib/utils/index.js')
-const createOnceAction = require('../lib/Once.js')
+const checkNodeVersion = require('../lib/utils/checkNodeVersion')
+const { createOnceAction } = require('../lib/Once.js')
+const { createMiAction } = require('../lib/mi.js')
+const { createMcAction } = require('../lib/mc.js')
+const { createInspectAction } = require('../lib/inspect')
+const { createRevokeAction } = require('../lib/revoke')
+const { createClearAction } = require('../lib/clear')
 const pkg = require(path.join(__dirname, '../package'))
 const program = new Command()
 
@@ -14,48 +19,36 @@ program
 
 program
     .command('once')
-    .description('Rename a single file')
-    .allowUnknownOption()
+    .description('Rename a single file(重命名一个单文件)')
     .action(createOnceAction)
 
 program
     .command('mi')
     .description('Rename multiple files by order(通过序号重命名多个文件)')
-    .allowUnknownOption()
-    .action(() => {
-        require(path.join(__dirname, '../lib/Mi.js'))
-    })
+    .action(createMiAction)
 
 program
     .command('mc')
     .description('Rename multiple files by template(通过模板重命名多个文件)')
     .allowUnknownOption()
-    .action(() => {
-        require(path.join(__dirname, '../lib/Mc.js'))
-    })
+    .action(createMcAction)
 
 program
     .command('clear')
-    .description('Clear cached file')
+    .description('Clear cached file（清除文件缓存）')
     .allowUnknownOption()
-    .action(() => {
-        require(path.join(__dirname, '../lib/Clear.js'))
-    })
+    .action(createClearAction)
 
 program
     .command('revoke')
-    .description('Undo rename operation')
+    .description('Undo rename operation（撤销重命名操作）')
     .allowUnknownOption()
-    .action(() => {
-        require(path.join(__dirname, '../lib/Revoke.js'))
-    })
+    .action(createRevokeAction)
 
 program
     .command('inspect')
-    .description('View current basic configuration')
+    .description('View current basic configuration(基础配置文件)')
     .allowUnknownOption()
-    .action(() => {
-        require(path.join(__dirname, '../lib/Inspect.js'))
-    })
+    .action(createInspectAction)
 
 program.parse(process.argv);
